7b459e670efb664e94c7620473c65f6a382f7851
Xiaobang Shi <xiaobang.shi@outlook.com>
2022-05-22 17:28:51 +0800

    Update build.py

--- build.py
+++ build.py
@@ -14,9 +14,9 @@ path = cwd + "\\build\\" + "软件过程与软件项目管理.md"
 mds = ["1.概论.md", "2.软件质量管理.md", "3.软件项目管理.md", "4.经典的软件过程管理.md", "5.敏捷软件开发.md"]
 
 if os.path.exists(cwd + "\\build"):
-    print(111111)
+    print("拼接文件中...")
     build(mds, path)
 else:
-    print(222222)
+    print("拼接文件中...")
     os.mkdir(cwd + "\\build")
     build(mds, path)
